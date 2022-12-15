17c4d0a268b86462ba25f6d31fd1b0b938fe4be6
Alejandro Gómez <josealejandrogomezparedes@gmail.com>
2022-12-15 11:09:35 -0500

    creo que terminada

--- curriculum/curriculum.py
+++ curriculum/curriculum.py
@@ -5,10 +5,17 @@ from datetime import datetime
 info={}
 info['fecha']=datetime.now().strftime('%A-%B-%Y')
 info['nombre']= 'José Alejandro'
-info['apellido']='Gómez Paredes'
-info['descripcion']=''
-info['educacion']='Estudios en curso: ingenieria en software'
-info['experiencia']='sin experiencia laboral'
+info['apellidos']='Gómez Paredes'
+info['cedula']='091243423'
+info['sexo']='M'
+info['nacimiento']='29/9/2022'
+info['email']= 'jgomezp4@unemi.edu.ec'
+info['estado']= 'soltero'
+info['domicilio']= ''
+info['cursos']= "CS50's Introduction to Computer Science"
+info['firmas']= 'Alejandro Gómez'
+info['estudios'] = 'colegio: x, escuela: x, universidad: Universidad estatal de Milagro'
+
 
 context = {'info':info}
 tempLoader = jinja2.FileSystemLoader('C:/Users/USUARIO/Music/Workspace/projects/curriculum-py/curriculum')
